f94756c33eaeba9ff82459484465b789b95a25f4
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-07-30 21:08:43 -0300

    SomaDeParesConsecutivos

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Exercício do site URI "Soma pares consecutivos"
+// Neste aplicativo o usuário irá digitar números inteiros até que seja digitado "0",
+// A partir do numero dogitado o programa somara os priximos 5 números pares, trazendo no final a soma deles.
+
+int Soma;
+
+int x = int.Parse(Console.ReadLine());
+
+while (x != 0)
+{
+    // se for impar, some mais 1
+    if (x % 2 != 0)
+    {
+        x = x + 1;
+    }
+    // expressao simplificada: 5 * x + 20
+    int soma = x + x + 2 + x + 4 + x + 6 + x + 8;
+    Console.WriteLine(soma);
+
+    x = int.Parse(Console.ReadLine());
+}
